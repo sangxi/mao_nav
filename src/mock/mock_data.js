db2d96dd50694e303de0ddd95640924441941c66
sangxi <93236239+sangxi@users.noreply.github.com>
2026-06-09 15:22:05 +0800

    chore: 更新导航数据 - 2026/6/9 15:22:07

--- src/mock/mock_data.js
+++ src/mock/mock_data.js
@@ -517,6 +517,7 @@ export const mockData = {
       ]
     }
   ],
-  "title": "猫猫导航🐱",
-  "search": "bing"
+  "title": "猫猫导航",
+  "search": "bing",
+  "_fileSha": "354bdf3300f231848bfa8f1cf09ea8077e6f712f"
 }
